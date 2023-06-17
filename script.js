49583593ce57df1f7c29ff760a6ce28a0c9f9b8d
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-06-17 04:24:34 +0000

    donef2js

--- script.js
+++ script.js
@@ -8,20 +8,47 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+
+  let arr1 = arr.filter((x) => x.profession === "developer");
+  arr1.map((x) => console.log(x));
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((x) => {
+    if (x.profession === "developer") {
+      console.log(x);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let obj = {
+    id: 4,
+    name: "susan",
+    age: "20",
+    profession: "intern",
+  };
+  arr.push(obj);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let removeObj = arr.filter((x) => x.profession !== "admin");
+  console.log(removeObj);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let arr3 = [
+    { id: 4, name: "johncena", age: "28", profession: "admin" },
+    { id: 5, name: "jackson", age: "26", profession: "developer" },
+    { id: 6, name: "karenGillen", age: "14", profession: "admin" },
+  ];
+
+  let res = arr.concat(arr3);
+  console.log(res);
 }
+
+
